572610d2c70dada8fa327f9a9a12235918d5d6c7
Ahmed Amr <ahmedbonumstelio@gmail.com>
2020-06-10 14:36:46 +0200

    get python3.8 instade of 3.7

--- getasd.py
+++ getasd.py
@@ -4,17 +4,18 @@ try:
     import setuptools
 except ModuleNotFoudError:
     print('installing setuptools...')
-    sp.call('pip3.7','install','setuptools')
+    sp.call('pip3.8','install','setuptools')
     import setuptools
 
 argv = os.sys.argv   
 dev = os.path.expanduser('~/Documents/dev') if len(argv) == 1 else argv[1]
+print(dev)
 if not os.path.isdir(dev):
     os.mkdir(dev)
 
 os.chdir(dev)
 print('ok')
 os.system("git clone https://github.com/theunderd0g/asd")
-sp.call(f'python3.7 {dev}/asd/setup.py develop'.split())
+sp.call(f'sudo python3 {dev}/asd/setup.py develop'.split())
 
 
